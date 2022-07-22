5b569124d159e54ebc304b5f151e612f28183358
sgf227 <1265968629@qq.com>
2022-07-22 10:39:06 +0800

    自动部署-测试一下是否成功

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            Docusaurus Tutorial - 7min ⏱️
+            Docusaurus Tutorial - 6min ⏱️
           </Link>
         </div>
       </div>
